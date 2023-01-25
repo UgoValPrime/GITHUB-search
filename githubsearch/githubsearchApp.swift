1a1b91328a7d4d5673a55b3feb67b4592a2ff322
Ikechukwu Onuorah <40242544+Lenever@users.noreply.github.com>
2023-01-25 02:08:17 +0100

    Initial Commit

---
+++ githubsearch/githubsearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubsearchApp.swift
+//  githubsearch
+//
+//  Created by Ikechukwu Onuorah on 25/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct githubsearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
